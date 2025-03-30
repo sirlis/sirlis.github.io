b8202a92fccc15973c570f3b10d15d16d300ead1
sirlis <jakeli@live.cn>
2025-03-31 03:54:27 +0800

    update post and add new post  SVM

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250329.181701';
+const cacheName = 'chirpy-20250331.035340';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
