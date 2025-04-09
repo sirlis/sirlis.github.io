b0d0529c293dd6ef78f57e7b4edacc0fbfa8c871
sirlis <jakeli@live.cn>
2025-04-09 20:57:54 +0800

    update post nolinear classifier

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250409.141116';
+const cacheName = 'chirpy-20250409.205605';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
