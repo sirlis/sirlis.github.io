182d55a0ca62430ff257f79ee18e4625f5c339d0
sirlis <jakeli@live.cn>
2025-04-10 18:26:26 +0800

    update RL policy gradient

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250409.205605';
+const cacheName = 'chirpy-20250410.182556';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
