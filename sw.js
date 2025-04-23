7bd5e24c61556083cda05b82c3cf81fafe20f65e
sirlis <jakeli@live.cn>
2025-04-23 18:54:51 +0800

    update pattern recognition - feature

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250418.185852';
+const cacheName = 'chirpy-20250423.184323';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
