2a189c735fd34412bddd3c9bdc8612bb042be0eb
sirlis <jakeli@live.cn>
2025-05-09 13:38:12 +0800

    update pca and lda

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250508.182414';
+const cacheName = 'chirpy-20250509.124150';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
