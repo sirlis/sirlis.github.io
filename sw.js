44021525d3ec6f04bf43475308d853d2897468a9
sirlis <jakeli@live.cn>
2025-05-28 14:09:14 +0800

    update post dnn

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250528.030145';
+const cacheName = 'chirpy-20250528.140806';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
