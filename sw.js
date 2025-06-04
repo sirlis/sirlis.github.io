3af162c00d1fa263efd8e8327708903e4af0b9b8
sirlis <jakeli@live.cn>
2025-06-04 20:49:35 +0800

    add new post cnn

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250603.105722';
+const cacheName = 'chirpy-20250604.204916';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
