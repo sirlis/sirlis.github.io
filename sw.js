6f76ac0f324533b84e607348166651939d2923fd
sirlis <jakeli@live.cn>
2025-07-14 14:09:57 +0800

    update astronomy coordinate

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250626.181801';
+const cacheName = 'chirpy-20250714.140904';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
