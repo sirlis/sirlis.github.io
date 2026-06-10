079ff52a36760ee1b71d85620dca0cf7965b3a31
sirlis <jakeli@live.cn>
2026-06-10 21:32:35 +0800

    update CNN post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260602.223618';
+const cacheName = 'chirpy-20260610.213123';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
