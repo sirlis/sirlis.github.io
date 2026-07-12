5a285864473ef2cc9bbe0b60f0f921399d4e08db
sirlis <jakeli@live.cn>
2026-07-12 17:02:42 +0800

    update search strategy

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260707.195050';
+const cacheName = 'chirpy-20260712.165943';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
