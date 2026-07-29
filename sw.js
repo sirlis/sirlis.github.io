f3e4614db6a5b4ff96f0948d7c15a939caf1118d
sirlis <jakeli@live.cn>
2026-07-29 18:43:45 +0800

    new post NLP-wordvec

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260712.165943';
+const cacheName = 'chirpy-20260729.184216';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
