432e441397a4b09ae4792d65c5d27d4cc056c2ec
sirlis <jakeli@live.cn>
2026-09-08 15:16:31 +0800

    update post welcome to jekyll

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260908.150938';
+const cacheName = 'chirpy-20260908.151601';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
