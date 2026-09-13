fbc22bde7cde10bd772b901797f3dbbde4acfffb
sirlis <jakeli@live.cn>
2026-09-13 13:58:56 +0800

    update posts

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260910.180058';
+const cacheName = 'chirpy-20260913.135831';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
