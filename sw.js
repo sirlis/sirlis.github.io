802546a797536e8e114815815e1a06852ae62901
sirlis <jakeli@live.cn>
2026-09-13 21:09:33 +0800

    update post knowledge representation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20260913.135831';
+const cacheName = 'chirpy-20260913.210855';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
